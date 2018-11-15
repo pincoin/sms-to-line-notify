ad91caa1306ee05f0e66a4513675f5251cefe3a6
pincoin <pincoins@gmail.com>
2018-11-16 01:44:23 +0900

    - import source files

---
+++ app/src/main/java/com/hfad/broadcastsms/LineNotifyAsyncTask.java
@@ -0,0 +1,47 @@
+package com.hfad.broadcastsms;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class LineNotifyAsyncTask extends AsyncTask<String, Void, Void> {
+    private static final String TAG = "LineNotifyAsyncTask";
+
+    @Override
+    protected Void doInBackground(String... params) {
+        String header = "Bearer " + BuildConfig.LINE_NOTIFY_TOKEN;
+
+        try {
+            String text = URLEncoder.encode(params[0], "UTF-8");
+            URL url = new URL("https://notify-api.line.me/api/notify");
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            con.setRequestProperty("Cache-Control", "no-cache");
+            con.setRequestProperty("Authorization", header);
+
+            String postParams = "message=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+
+            if (con.getResponseCode() != 200) {
+                Log.e(TAG, "failed to send LINE notify");
+            }
+
+            con.disconnect();
+        } catch (Exception ex) {
+            Log.e(TAG, "failed to connect LINE notify");
+            Log.e(TAG, ex.toString());
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/hfad/broadcastsms/Receiver.java
@@ -0,0 +1,49 @@
+package com.hfad.broadcastsms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class Receiver extends BroadcastReceiver {
+    private static final String TAG = "Receiver";
+
+    private static final String KOOKMIN = "16449999";
+    private static final String NONGHUP = "15882100";
+    private static final String SHINHAN = "15778000";
+    private static final String WOORI = "15885000";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LineNotifyAsyncTask asyncTask = new LineNotifyAsyncTask();
+
+        if ("android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) {
+            Bundle bundle = intent.getExtras();
+            Object[] messages = (Object[]) bundle.get("pdus");
+            SmsMessage[] smsMessage = new SmsMessage[messages.length];
+
+            for (int i = 0; i < messages.length; i++) {
+                smsMessage[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
+            }
+
+            String phone = smsMessage[0].getOriginatingAddress();
+
+            if (phone.equals(KOOKMIN) || phone.equals(NONGHUP) || phone.equals(SHINHAN) || phone.equals(WOORI)) {
+                phone = "은행";
+            }
+
+            asyncTask.execute(phone + " " + smsMessage[0].getMessageBody());
+        } else if ("android.intent.action.BATTERY_LOW".equals(intent.getAction())) {
+            asyncTask.execute("배터리 부족");
+        } else if ("android.intent.action.BATTERY_OKAY".equals(intent.getAction())) {
+            asyncTask.execute("배터리 정상화");
+        } else if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+            Intent intentMainActivity = new Intent(context, MainActivity.class);
+            intentMainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intentMainActivity);
+
+            asyncTask.execute("부팅 완료");
+        }
+    }
+}
