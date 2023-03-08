595c15188eaa468005ceeef31b440dd2b7cc0a0b
pincoin <pincoins@gmail.com>
2023-03-08 11:09:18 +0900

    power management doze mode

--- app/src/main/java/kr/co/pincoin/broadcastsms/MainActivity.java
+++ app/src/main/java/kr/co/pincoin/broadcastsms/MainActivity.java
@@ -1,7 +1,12 @@
 package kr.co.pincoin.broadcastsms;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        String packageName = getPackageName();
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            // 메모리 최적화가 되어 있다면, 풀기 위해 설정 화면 띄움.
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + packageName));
+            startActivityForResult(intent, 0);
+        }
     }
 }
